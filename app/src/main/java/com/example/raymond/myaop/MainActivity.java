6957384cd0653a45e199383597b136fa12bc1945
Raymond <raymondctc@me.com>
2016-06-19 16:16:10 +0800

    Added list item

--- app/src/main/java/com/example/raymond/myaop/MainActivity.java
+++ app/src/main/java/com/example/raymond/myaop/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.raymond.myaop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+
+        final String[] posts = new String[] {
+                "Post 1",
+                "Post 2",
+                "Post 3",
+                "Post 4",
+                "Post 5"
+        };
+
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<>(
+                this,
+                R.layout.post_item,
+                R.id.textView,
+                posts
+        );
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: clicked Post= " + position);
+            }
+        });
     }
+
+
 }
